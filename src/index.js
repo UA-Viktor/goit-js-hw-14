8987ee57d2f3c78517d1b57488f6000e69120e5c
UA-Viktor <v.korenkov@icloud.com>
2023-02-27 16:11:08 +0200

    update

--- src/index.js
+++ src/index.js
@@ -17,21 +17,46 @@ function onSearch(e) {
 
     fotoServiceApi.query = e.currentTarget.elements.searchQuery.value;
     fotoServiceApi.resetPage(); 
-    fotoServiceApi.fotoArticles();
+    fotoServiceApi.fotoArticles().then(({ data }) => refs.container.innerHTML = renderFotoCard(data.hits))
+// 
+    // fotoServiceApi.fotoArticles().then(data => {renderFotoCard(data)});
+    // fotoServiceApi.fotoArticles();
+
+        // .then(hits => {
+        //     refs.container.innerHTML = renderFotoCard(hits);
+        // }
+            
+        // );
+    
 }
 
 function onLoadMore() {
     fotoServiceApi.fotoArticles();
 }
 
-
-const user = {
-    name: 'Viktor',
-    age: 36,
-}
-
-// function renderFotoCard(fotoCardJSON) {
-//     refs.container.innerHTML = fotoCardJSON(fotoCardJSON);
-// }
-
-// renderFotoCard(user);
+function renderFotoCard(searchData) {
+    return searchData.map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+        return `
+            <div class="photo-card">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+                <p class="info-item">
+                <b>Likes</b>
+                ${likes}
+                </p>
+                <p class="info-item">
+                <b>Views</b>
+                ${views}
+                </p>
+                <p class="info-item">
+                <b>Comments</b>
+                ${comments}
+                </p>
+                <p class="info-item">
+                <b>Downloads</b>
+                ${downloads}
+                </p>
+            </div>
+        </div>
+    ` }).join('');
+};
--- src/js/foto-service-api.js
+++ src/js/foto-service-api.js
@@ -13,16 +13,17 @@ export default class FotoServiceApi{
         const KEY = '33842682-eca7b00e850c4a3ea440c3225';
         const OPTIONS = 'image_type=photo&orientation=horizontal&safesearch=true&per_page=40';
 
-        try {
-                // return fetch(`${BASE_URL}/?key=${KEY}&q=${this.searchQuery}&${OPTIONS}&page=${this.page}`).then(r => r.json()).then(console.log)
+            // return fetch(`${BASE_URL}/?key=${KEY}&q=${this.searchQuery}&${OPTIONS}&page=${this.page}`)
+            //     .then(r => {
+            //     this.incrementPage();
+            //     r.json();
+            // })
                 return await axios.get(`${BASE_URL}/?key=${KEY}&q=${this.searchQuery}&${OPTIONS}&page=${this.page}`)
                     .then(response => {
                         this.incrementPage();
-                        console.log(response.data);;
+                        response;
                     });
-            } catch {
-                console.error(error);
-            }
+            
     };
 
     incrementPage() {
